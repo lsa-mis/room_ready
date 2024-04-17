ab296a615675eb0b37b165cd41e97fb0cbd832d6
Margarita Barvinok <brita@umich.edu>
2024-04-16 22:48:04 -0400

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  host = 'http://localhost:3000'
+  # on staging use host = 'http://STAGING SERVER NAME'
+  config.action_mailer.default_url_options = { host: host }
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
