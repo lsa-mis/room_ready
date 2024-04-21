356d3b2d61424b1ac6c7f7df069679aa2839b295
Margarita Barvinok <brita@umich.edu>
2024-04-21 19:25:48 -0400

    edit home page

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
+    console.log("connect")
     setTimeout(() => { this.dismiss(); }, 5000)
   }
   dismiss() {
