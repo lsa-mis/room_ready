d05aad3ed5e5f3677da64655d29a1bd0c3a65347
Margarita Barvinok <brita@umich.edu>
2024-05-17 20:36:20 -0400

    add building specs

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -17,5 +17,58 @@
 require 'rails_helper'
 
 RSpec.describe Building, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "the Factory" do
+    it 'is valid' do
+      expect(build(:building)).to be_valid
+    end
+  end
+
+  context "create building with all required fields present" do
+    it 'is valid' do
+      expect(create(:building, nick_name: nil)).to be_valid
+    end
+  end
+
+  context "create building without a bldrecnbr" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Bldrecnbr can\'t be blank"' do
+      expect { FactoryBot.create(:building, bldrecnbr: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Bldrecnbr can't be blank")
+    end
+  end
+
+  context "create building without a name" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Name can\'t be blank"' do
+      expect { FactoryBot.create(:building, name: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+    end
+  end
+
+  context "create building without an address" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Address can\'t be blank"' do
+      expect { FactoryBot.create(:building, address: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Address can't be blank")
+    end
+  end
+
+  context "create building without a city" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: City can\'t be blank"' do
+      expect { FactoryBot.create(:building, city: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: City can't be blank")
+    end
+  end
+
+  context "create building without a state" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: State can\'t be blank"' do
+      expect { FactoryBot.create(:building, state: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: State can't be blank")
+    end
+  end
+
+  context "create building without a zip" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Zip can\'t be blank"' do
+      expect { FactoryBot.create(:building, zip: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Zip can't be blank")
+    end
+  end
+
+  context "create building without a zone" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Zone must exist"' do
+      expect { FactoryBot.create(:building, zone: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Zone must exist")
+    end
+  end
+
 end
