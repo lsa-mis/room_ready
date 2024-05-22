1b02d17cccf43574d910c88c8b6d1d3b961dac35
Mohammad <mohammadimaan484@gmail.com>
2024-05-22 13:09:21 -0400

    removed status field from resources table, and all references to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_181256) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_170406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -122,7 +122,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_181256) do
   create_table "resources", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.string "status"
     t.bigint "room_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
