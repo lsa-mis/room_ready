537670f8aa3cc4e3ddcb812d0329e528f32ecce7
Margarita Barvinok <brita@umich.edu>
2024-05-22 16:16:37 -0400

    add about_page spec

---
+++ spec/system/about_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'About page', type: :system do
+  describe 'about page' do
+    it 'shows the right content' do
+      visit root_path
+      sleep(2)
+      expect(page).to have_content('About Room Ready Application')
+    end
+  end
+end
