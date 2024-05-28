96420fe1caf3a3a05821bc373dfa68e4c9c7bdbb
Charlie <zfzheng@umich.edu>
2024-05-28 12:28:27 -0400

    added rspec test for rover page, finished with announcement implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def render_flash_stream
     turbo_stream.update "flash", partial: "layouts/flash"
   end
+
+  def show_date(field)
+    field.strftime("%m/%d/%Y") unless field.blank?
+  end
+
+  def show_date_with_month_name(field)
+    field.to_date.strftime("%B %d, %Y") unless field.blank?
+  end
 end
