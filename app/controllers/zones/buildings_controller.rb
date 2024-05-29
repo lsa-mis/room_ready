b9b8dda3e72404d089f2de61f983ed216c71b3f9
Margarita Barvinok <brita@umich.edu>
2024-05-28 21:33:37 -0400

    update Zones::BuildingsController

--- app/controllers/zones/buildings_controller.rb
+++ app/controllers/zones/buildings_controller.rb
@@ -1,6 +1,6 @@
 class Zones::BuildingsController < ApplicationController
   before_action :auth_user
-  before_action :set_building, only: %i[ remove_building ]
+  before_action :set_building, only: %i[ edit update remove_building ]
   before_action :set_zone
 
 
@@ -20,32 +20,35 @@ class Zones::BuildingsController < ApplicationController
 
   def create
     @building = Building.new(bldrecnbr: building_params[:bldrecnbr], zone_id: params[:zone_id])
-    authorize([@zone, @building]) 
-    if @building.save
-      format.html { redirect_to zone_buildings_path, notice: "Building was added" }
-      format.json { render :show, status: :ok, location: @building }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @building.errors, status: status }
+    authorize([@zone, @building])
+    respond_to do |format|
+      if @building.save
+        notice = "New Building was added to the zone."
+        @buildings = Building.where(zone: @zone)
+        format.turbo_stream do
+          @new_building = Building.new
+          redirect_to zone_buildings_path, notice: notice
+        end
+        format.html { redirect_to zone_buildings_path, notice: notice }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
-    # if @zone.buildings << @building
-    #   fail
-    #   @building = Building.new
-    #   @buildings = Building.where(zone: @zone)
-    #   flash.now[:notice] = "The building was added."
-    # else
-    #   fail
-    # end
-    
   end
 
+  def edit
+    @zones = Zone.all.map { |z| [z.name, z.id] }
+    authorize([@zone, @building])
+  end
+    
   # PATCH/PUT /buildings/1 or /buildings/1.json
   def update
-    @building_params = building_params
-    @zone_id = @building_params[:zone_id]
-
-    success = @building.update(building_params)
-    respond_with_notice(success, zone_building_path(@zone_id, @building), "Building was successfully updated")
+    authorize([@zone, @building])
+    if @building.update(building_params)
+      redirect_to zone_buildings_path, notice: "Building was successfully updated."
+    else 
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /buildings/1 or /buildings/1.json
@@ -53,23 +56,23 @@ class Zones::BuildingsController < ApplicationController
     authorize([@zone, @building])
     if @zone.buildings.delete(@building)
       @buildings = Building.where(zone: @zone)
-      flash.now[:notice] = "The building was removed from the program."
+      flash.now[:notice] = "The building was removed from the zone."
     end
     @building = Building.new
   end
 
   private
-    def respond_with_notice(success, redirect, notice_text)
-      respond_to do |format|
-        if success
-          format.html { redirect_to redirect, notice: notice_text }
-          format.json { render :show, status: :ok, location: @building }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @building.errors, status: status }
-        end
-      end
-    end
+    # def respond_with_notice(success, redirect, notice_text)
+    #   respond_to do |format|
+    #     if success
+    #       format.html { redirect_to redirect, notice: notice_text }
+    #       format.json { render :show, status: :ok, location: @building }
+    #     else
+    #       format.html { render :edit, status: :unprocessable_entity }
+    #       format.json { render json: @building.errors, status: status }
+    #     end
+    #   end
+    # end
 
     # Only allow a list of trusted parameters through.
     def building_params
