b7530117973b968ddf3b3ce76b4d237070b1ee5e
Satyadev Moolagani <smoolagani@gmail.com>
2024-05-30 09:28:45 -0400

    cleaned up zone_building controller file

--- app/controllers/zones/buildings_controller.rb
+++ app/controllers/zones/buildings_controller.rb
@@ -21,44 +21,9 @@ class Zones::BuildingsController < ApplicationController
 
   def create
     if params[:building_id].present?
-      @building = Building.find(params[:building_id])
-      authorize([@zone, @building]) 
-      if @zone.buildings << @building
-        @building = Building.new
-        @buildings = Building.where(zone: @zone)
-        flash.now[:notice] = "The building was added."
-        return
-      end
+      return add_building_from_existing
     elsif building_params[:bldrecnbr].present?
-      note = ""
-      bldrecnbr = building_params[:bldrecnbr]
-      @building = Building.new(bldrecnbr: bldrecnbr, zone_id: params[:zone_id])
-      building_data = get_building_info_by_bldrecnbr(bldrecnbr)
-      if building_data['data'].present?
-        data = building_data['data'].first
-        @building.name = data['BuildingLongDescription']
-        @building.address = "#{data['BuildingStreetNumber']}  #{data['BuildingStreetDirection']}  #{data['BuildingStreetName']}".strip.gsub(/\s+/, " ")
-        @building.city = data['BuildingCity']
-        @building.state = data['BuildingState']
-        @building.zip = data['BuildingPostal']
-      else 
-        note = " API returned no data about #{bldrecnbr} building"
-      end
-    end
-
-    authorize([@zone, @building])
-    respond_to do |format|
-      if @building.save
-        notice = "New Building was added to the zone." + note
-        @buildings = Building.where(zone: @zone)
-        format.turbo_stream do
-          @new_building = Building.new
-          redirect_to zone_buildings_path, notice: notice
-        end
-        format.html { redirect_to zone_buildings_path, notice: notice }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+      return add_from_bldrecnbr
     end
   end
 
@@ -88,18 +53,6 @@ class Zones::BuildingsController < ApplicationController
   end
 
   private
-    # def respond_with_notice(success, redirect, notice_text)
-    #   respond_to do |format|
-    #     if success
-    #       format.html { redirect_to redirect, notice: notice_text }
-    #       format.json { render :show, status: :ok, location: @building }
-    #     else
-    #       format.html { render :edit, status: :unprocessable_entity }
-    #       format.json { render json: @building.errors, status: status }
-    #     end
-    #   end
-    # end
-
     # Only allow a list of trusted parameters through.
     def building_params
       params.require(:building).permit(:bldrecnbr, :name, :nick_name, :abbreviation, :address, :city, :state, :zip, :zone_id)
@@ -113,5 +66,47 @@ class Zones::BuildingsController < ApplicationController
     def set_zone
       @zone_id = params[:zone_id]
       @zone = Zone.find(params[:zone_id])
+    end
+
+    def add_building_from_existing
+      @building = Building.find(params[:building_id])
+      authorize([@zone, @building]) 
+      if @zone.buildings << @building
+        @building = Building.new
+        @buildings = Building.where(zone: @zone)
+        flash.now[:notice] = "The building was added."
+      end
+    end
+
+    def add_from_bldrecnbr
+      note = ""
+      bldrecnbr = building_params[:bldrecnbr]
+      @building = Building.new(bldrecnbr: bldrecnbr, zone_id: params[:zone_id])
+      building_data = get_building_info_by_bldrecnbr(bldrecnbr)
+      if building_data['data'].present?
+        data = building_data['data'].first
+        @building.name = data['BuildingLongDescription']
+        @building.address = "#{data['BuildingStreetNumber']}  #{data['BuildingStreetDirection']}  #{data['BuildingStreetName']}".strip.gsub(/\s+/, " ")
+        @building.city = data['BuildingCity']
+        @building.state = data['BuildingState']
+        @building.zip = data['BuildingPostal']
+      else 
+        note = " API returned no data about #{bldrecnbr} building"
+      end
+
+      authorize([@zone, @building])
+      respond_to do |format|
+        if @building.save
+          notice = "New Building was added to the zone." + note
+          @buildings = Building.where(zone: @zone)
+          format.turbo_stream do
+            @new_building = Building.new
+            redirect_to zone_buildings_path, notice: notice
+          end
+          format.html { redirect_to zone_buildings_path, notice: notice }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
     end
 end
