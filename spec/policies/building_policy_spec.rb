39f051652f46ea06f02604887eb5e96fa3b742e7
Margarita Barvinok <brita@umich.edu>
2024-06-01 19:27:52 -0400

    add BuildingPolicy spec

--- spec/policies/building_policy_spec.rb
+++ spec/policies/building_policy_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe BuildingPolicy do
-  subject { described_class.new(user, building) }
-
+  let(:user) { FactoryBot.create(:user) }
   let(:building) { Building.new }
 
   context 'with rovers' do
-    let(:rover) { FactoryBot.create(:rover) }
-    let(:user) { FactoryBot.create(:user, uniqname: rover.uniqname) }
+    subject { described_class.new({ user: user, role: "rover" }, building) }
 
-    it { is_expected.to permit_only_actions(%i[index show]) }
+    it { is_expected.to permit_only_actions(%i[index show is_rover]) }
   end
 
   context 'with admins' do
-    let(:user) { FactoryBot.create(:user, admin: true, membership: ['lsa-roomready-admins']) }
+    subject { described_class.new({ user: user, role: "admin" }, building) }
 
-    # it { is_expected.to permit_all_actions }
+    it { is_expected.to forbid_action(:is_rover) }
+  end
+
+  context 'with admins' do
+    subject { described_class.new({ user: user, role: "developer" }, building) }
+
     it { is_expected.to forbid_action(:is_rover) }
   end
 
   context 'with rovers' do
-    let(:user) { FactoryBot.create(:user) }
+    subject { described_class.new({ user: user, role: "none" }, building) }
 
     it { is_expected.to forbid_all_actions }
   end
