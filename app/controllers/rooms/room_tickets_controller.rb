d7ee9abf69f41b64d2f132256101314d97824b1e
Satyadev Moolagani <smoolagani@gmail.com>
2024-06-07 08:25:37 -0400

    cleaned up controller file

--- app/controllers/rooms/room_tickets_controller.rb
+++ app/controllers/rooms/room_tickets_controller.rb
@@ -16,17 +16,15 @@ class Rooms::RoomTicketsController < ApplicationController
   end
 
   def send_email_for_tdx_ticket
-    date = Time.now.strftime('%m/%d/%Y')
     message = params.require(:room_ticket).permit(:description)[:description]
     submitter = current_user
-    room = @room
 
-    @room_ticket = RoomTicket.new(description: message, submitted_at: date, room_id: room.id, submitted_by: submitter )
+    @room_ticket = RoomTicket.new(description: message, room_id: @room.id, submitted_by: submitter )
     authorize @room_ticket
 
     respond_to do |format|
       if @room_ticket.save
-        RoomTicketMailer.with(date: date, room: room, message: message, submitter: submitter).send_tdx_ticket.deliver_now
+        RoomTicketMailer.with(date: @room_ticket.created_at.strftime('%m/%d/%Y'), room: @room, message: message, submitter: submitter).send_tdx_ticket.deliver_now
         format.html { redirect_to room_room_tickets_path, notice: "Room ticket was successfully sent." }
         format.json { render :show, status: :created, location: @room_ticket }
       else
