7ec43095573a980063226a42b6379e00cf426a72
Margarita Barvinok <brita@umich.edu>
2024-06-13 18:16:55 -0400

    edit building test

--- spec/system/create_new_building_spec.rb
+++ spec/system/create_new_building_spec.rb
@@ -5,27 +5,21 @@ RSpec.describe Building, type: :system do
   before do
 		user = FactoryBot.create(:user)
 		mock_login(user)
-    allow_any_instance_of(User).to receive(:membership).and_return(['lsa-roomready-admins'])
-	end
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-developers').and_return(false)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-admins').and_return(true)
+    session[:role] = "admin"
+  end
 
 	context 'create a new building' do
     it 'fills out the form and submit it' do
       VCR.use_cassette "LdapLookup.is_member_of_group?" do
         visit new_building_path
         sleep 2
-        fill_in "Bldrecnbr", with: "1234567"
-        fill_in "Name", with: "Chemistry"
-        fill_in "Nick name", with: "Chem"
-        fill_in "Address", with: "123 N.University St."
-        fill_in "City", with: "Ann Arbor"
-        fill_in "State", with: "MI"
-        fill_in "Zip", with: "48109"
-        sleep 5
+        fill_in "Building Record Number", with: "1234567"
         click_on "Create Building"
-        sleep 5
-        expect(page).to have_content("Building was successfully created.")
+        expect(page).to have_content("is not valid.")
+        sleep 2
       end
     end
   end
-
 end
