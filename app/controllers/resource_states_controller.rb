7d50a1570094118f1a125eed2d8b924fddebe652
Margarita Barvinok <brita@umich.edu>
2024-06-13 16:21:39 -0400

    resource states forms

--- app/controllers/resource_states_controller.rb
+++ app/controllers/resource_states_controller.rb
@@ -1,7 +1,6 @@
 class ResourceStatesController < ApplicationController
   before_action :auth_user
-  before_action :set_resource_state, only: %i[ edit ]
-  before_action :set_room, only: %i[ new create ]
+  before_action :set_room, only: %i[ new create edit update_resource_states]
 
   # GET /resource_states/new
   def new
@@ -13,13 +12,15 @@ class ResourceStatesController < ApplicationController
   end
 
   def edit
+    @resource_states = @room_state.resource_states
+    authorize @resource_states
   end
 
   # POST /resource_states or /resource_states.json
   def create
     authorize ResourceState
 
-    @room_state = @room.room_state_for_today
+    # @room_state = @room.room_state_for_today
     @resource_states = resource_state_params.map do |res_params|
       @room_state.resource_states.new(res_params)
     end
@@ -37,33 +38,47 @@ class ResourceStatesController < ApplicationController
     end
   end
 
+  def update_resource_states
+    authorize ResourceState
+    resource_state_params.each do |res_params|
+      resource_state = ResourceState.find(res_params[:resource_state_id])
+      # raise ActiveRecord::Rollback unless resource_state.update(res_params)
+      unless resource_state.update(res_params.except(:resource_state_id))
+        render :edit, status: :unprocessable_entity
+        return
+      end
+    end
+
+    # if @resource_states.all?(&:persisted?)
+      redirect_to new_room_room_ticket_path(@room), notice: 'Resources States were successfully saved.'
+    # else
+    #   render :edit, status: :unprocessable_entity
+    # end
+  end
 
   private
 
-  def set_resource_state
-    @resource_state = ResourceState.find(params[:id])
-  end
-
   def set_room
-    @room = Room.find_by(id: params[:room_id])
+    @room_state = RoomState.find(params[:room_state_id])
+      @room = @room_state.room
 
     unless @room
       redirect_to rooms_path, alert: 'Room doesnt exist.' and return
     end
 
-    room_state = @room.room_state_for_today
-    if room_state.nil?
-      redirect_to room_path(@room), alert: 'Complete previous steps for Room.'
-    elsif room_state.resource_states.any?
-      redirect_to room_path(@room), alert: 'Already saved Common Attribute States for this Room today.'
-    end
+    # room_state = @room.room_state_for_today
+    # if room_state.nil?
+    #   redirect_to room_path(@room), alert: 'Complete previous steps for Room.'
+    # elsif room_state.resource_states.any?
+    #   redirect_to room_path(@room), alert: 'Already saved Common Attribute States for this Room today.'
+    # end
 
   end
 
   # Only allow a list of trusted parameters through.
   def resource_state_params
     params.require(:resource_states).values.map do |res_param|
-      res_param.permit(:is_checked, :room_state_id, :resource_id)
+      res_param.permit(:is_checked, :room_state_id, :resource_id, :resource_state_id)
     end
   end
 end
--- app/policies/resource_state_policy.rb
+++ app/policies/resource_state_policy.rb
@@ -1,10 +1,18 @@
 class ResourceStatePolicy < ApplicationPolicy
 
   def create?
-    is_rover? || is_admin?
+    is_rover?
    end
  
    def new?
      create?
    end
+
+   def update_resource_states?
+    is_rover?
+  end
+
+  def edit?
+    update_resource_states?
+  end
 end
