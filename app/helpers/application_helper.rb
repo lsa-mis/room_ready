7d588b88c1adc268f57218eac04fb025c905ed88
Margarita Barvinok <brita@umich.edu>
2024-06-18 01:41:19 -0400

    show room status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::NumberHelper
 
   def root_path
     if user_signed_in?
@@ -16,6 +18,43 @@ module ApplicationHelper
 
   def render_flash_stream
     turbo_stream.update "flash", partial: "layouts/flash"
+  end
+
+  def status_weight(room)
+    ca = CommonAttribute.count > 0 ? 1 : 0
+    sa = room.specific_attributes.count > 0 ? 1 : 0
+    res = room.resources.count 0 ? 1 : 0
+    w = 1.to_f / (1 + ca + sa + res) * 100
+    return w
+  end
+
+  def show_status(room)
+    if room.last_time_checked.present?
+      last_time_checked = room.last_time_checked.to_date
+      tomorrow = Date.today + 1.day
+      yesterday = Date.today - 1.day
+      if (yesterday..tomorrow).include?(last_time_checked)
+        # room is checked today
+        room_state = room.room_states.last
+        w = status_weight(room)
+        percentage = w
+        if CommonAttribute.count > 0
+          percentage += w if room_state.common_attribute_states.last.present? && (yesterday..tomorrow).include?(room_state.common_attribute_states.last.updated_at.to_date)
+        end
+        if room.specific_attributes.count > 0
+          percentage += w if room_state.specific_attribute_states.last.present? && (yesterday..tomorrow).include?(room_state.specific_attribute_states.last.updated_at.to_date)
+        end
+        if room.resources.count > 0
+          percentage += w if room_state.resources_states.last.present? && (yesterday..tomorrow).include?(room_state.resources_states.last.updated_at.to_date)
+        end
+        checked = "Checked " + number_with_precision(percentage, precision: 2).to_s + "%"
+      else
+        checked = "Checked " + time_ago_in_words(last_time_checked + 1.day) + " ago"
+      end
+    else
+      checked = "Never checked"
+    end
+    return checked
   end
 
   def us_states
