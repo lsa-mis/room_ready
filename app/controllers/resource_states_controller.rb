7c84138f8a90509a60b0e1c4acf372c120da0c5f
Satyadev Moolagani <smoolagani@gmail.com>
2024-06-21 11:03:04 -0400

    removed resource state function

--- app/controllers/resource_states_controller.rb
+++ app/controllers/resource_states_controller.rb
@@ -85,8 +85,4 @@ class ResourceStatesController < ApplicationController
       res_param.permit(:is_checked, :room_state_id, :resource_id, :resource_state_id)
     end
   end
-
-  def recommended_next_room_check
-    
-  end
 end
