33b01dd5d610d1d854af1780c68b998e73298ef9
Margarita Barvinok <brita@umich.edu>
2024-06-21 09:02:02 -0400

    temp

---
+++ spec/system/specific_attributes_controller_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe SpecificAttribute, type: :system do
+
+  before do
+		user = FactoryBot.create(:user)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-developers').and_return(false)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-admins').and_return(true)
+    mock_login(user)
+	end
+
+	context 'create a new specific attribute' do
+    let!(:room) { FactoryBot.create(:room) }
+    it 'fills out the form and submits it' do
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        fill_in "Description", with: "specific attribute one"
+        check "Needs Checkbox?"
+        click_on "Create"
+        expect(page).to have_content("Specific attribute was successfully created.")
+      end
+    end
+  end
+
+  context 'validate a new specific attribute' do
+    let!(:room) { FactoryBot.create(:room) }
+    it 'fills out the form and submits it' do
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        fill_in "Description", with: "specific attribute one"
+        click_on "Create"
+        expect(page).to have_content("Needs to have either a checkbox or a quantity box, or both.")
+      end
+    end
+  end
+
+  context 'edit a specific attribute' do
+    let!(:room) { FactoryBot.create(:room) }
+    let!(:specific_attribute) { FactoryBot.create(:specific_attribute) }
+
+    it 'click on edit icon and go to edit page' do
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        find(:css, 'i.bi.bi-pencil-square.text-primary').click
+        expect(page).to have_content("Edit Specific Attribute")
+      end
+    end
+
+    it 'click on edit icon and cancel editing' do
+      let!(:room) { FactoryBot.create(:room) }
+      visit "rooms/#{room.id}/specific_attributes"
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        find(:css, 'i.bi.bi-pencil-square.text-primary').click
+        expect(page).to have_content("Edit Specific Attribute")
+        click_on "Cancel"
+        expect(page).to have_content(specific_attribute.description)
+      end
+    end
+
+    it 'click on edit icon and update description' do
+      VCR.use_cassette "specific_attribute" do
+        visit specific_attributes_path
+        find(:css, 'i.bi.bi-pencil-square.text-primary').click
+        expect(page).to have_content("Edit Specific Attribute")
+        fill_in "Description", with: specific_attribute.description + "edited"
+        click_on "Update"
+        expect(page).to have_content(specific_attribute.description + "edited")
+      end
+    end
+
+    it 'click on delete icon and cancel the alert messege' do
+      VCR.use_cassette "specific_attribute" do
+        visit specific_attributes_path
+        dismiss_confirm 'Are you sure you want to delete this specific Attribute?' do
+          find(:css, 'i.bi.bi-trash-fill.text-danger').click
+        end
+        expect(page).to_not have_content("specific attribute was successfully deleted.")
+      end
+    end
+
+    it 'click on delete icon and accept the alert message' do
+      VCR.use_cassette "specific_attribute" do
+        visit specific_attributes_path
+        accept_confirm 'Are you sure you want to delete this specific Attribute?' do
+          find(:css, 'i.bi.bi-trash-fill.text-danger').click
+        end
+        expect(page).to have_content("Specific attribute was successfully deleted.")
+      end
+    end
+  end
+  
+end
---
+++ spec/system/specific_attributes_user_is_not_authorized_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe SpecificAttribute, type: :system do
+
+  before do
+		user = FactoryBot.create(:user)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-developers').and_return(false)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-admins').and_return(false)
+    mock_login(user)
+  end
+
+	context 'create a new specific_attribute' do
+    let!(:room) { FactoryBot.create(:room) }
+    it 'returns a "You are not authorized to perform this action." message' do
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        expect(page).to have_content("You are not authorized to perform this action.")
+      end
+      room.delete
+    end
+  end
+
+  context 'edit a specific_attribute' do
+    let!(:room) { FactoryBot.create(:room) }
+    let!(:specific_attribute) { FactoryBot.create(:specific_attribute) }
+
+    it 'returns a "You are not authorized to perform this action." message' do
+      VCR.use_cassette "specific_attribute" do
+        visit "rooms/#{room.id}/specific_attributes"
+        binding.pry
+        find(:css, 'i.bi.bi-pencil-square.text-primary').click
+        expect(page).to have_content("You are not authorized to perform this action.")
+      end
+    end
+  end
+  
+end
