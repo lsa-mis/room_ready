e8b247ce8d4fd78d8c747f578d5f4575c0f0ee92
Margarita Barvinok <brita@umich.edu>
2024-06-22 23:38:10 -0400

    add preferences to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,20 @@ locations.each do |location|
     Announcement.create!(location: location)
   end
 end
+
+preferences = [
+  {:name => 'no_access_reason', :description => "Comma-separated list of why you can't access a room", :pref_type => 'string'},
+  {:name => 'supervisor_phone', :description => 'A phone to report to a supervisor', :pref_type => 'string'},
+  {:name => 'resource_types', :description => 'Comma-separated list of resource types to add from WebCheckout', :pref_type => 'string'},
+  {:name => 'tdx_lsa_ts_email', :description => 'An email address to create a TDX ticket with LSA TS', :pref_type => 'string', :value => "Technology Issues (all classrooms): LSATechnologyServices@umich.edu"},
+  {:name => 'tdx_facilities_email', :description => 'An email address to create a TDX ticket with LSA Facilities', :pref_type => 'string', :value => "Facilities Issues (LSA classrooms): lsa-facilities@umich.edu"},
+  {:name => 'dana_building_facility_issues_email', :description => 'An email address to report Dana Building Facility Issues', :pref_type => 'string', :value => "Dana Building Facility Issues: Email seas-facilities@umich.edu"},
+  {:name => 'skb_facility_issues_email', :description => 'An email address to report SKB Building Facility Issues', :pref_type => 'string', :value => "SKB Facility Issues: Email facility manager ptitus@umich.edu"}
+]
+existing_preferences = AppPreference.all.pluck(:name)
+
+preferences.each do |pref|
+  unless existing_preferences.include?(pref[:name])
+    AppPreference.create!(pref)
+  end
+end
