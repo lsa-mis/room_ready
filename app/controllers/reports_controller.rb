41ff9e1f7a01e0dca14ac25e30295ed6f50d35e6
Mohammad <mohammadimaan484@gmail.com>
2024-06-25 10:02:28 -0400

    added to nav, added report type param, report doesnt run unless form submitted, minor ui changes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,15 +13,17 @@ class ReportsController < ApplicationController
     start_time = params[:from].present? ? Date.parse(params[:from]).beginning_of_day : Date.new(0)
     end_time = params[:to].present? ? Date.parse(params[:to]).end_of_day : Date::Infinity.new
 
-    @rooms = Room.joins(floor: :building).joins(:room_tickets)
-                 .where(buildings: { zone_id: params[:zone_id].presence ? params[:zone_id].presence : Zone.all.pluck(:id).push(nil) })
-                 .where(room_tickets: { created_at: start_time..end_time })
-                 .group('rooms.id')
-                 .select('rooms.*, COUNT(room_tickets.id) AS tickets_count')
-                 .having('COUNT(room_tickets.id) > 0')
-                 .order('tickets_count DESC')
+    if params[:commit]
+      @rooms = Room.joins(floor: :building).joins(:room_tickets)
+                   .where(buildings: { zone_id: params[:zone_id].presence ? params[:zone_id].presence : Zone.all.pluck(:id).push(nil) })
+                   .where(room_tickets: { created_at: start_time..end_time })
+                   .group('rooms.id')
+                   .select('rooms.*, COUNT(room_tickets.id) AS tickets_count')
+                   .having('COUNT(room_tickets.id) > 0')
+                   .order('tickets_count DESC')
 
-    @total_tickets_count = @rooms.sum(&:tickets_count)
+      @total_tickets_count = @rooms.sum(&:tickets_count)
+    end
 
     @zones = Zone.all.order(:name).map { |z| [z.name, z.id] }
   end
