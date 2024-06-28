05bd95d771739ab71a740256af9eaf617ed40098
Palina <palinaskakun@gmail.com>
2024-06-28 08:31:36 -0400

    calendar date change works only for piechart data. moved helper methods to dashboard helper

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,14 @@ class DashboardsController < ApplicationController
   before_action :auth_user
 
   def index
+
+    selected_date = if params[:dashboard_date].present?
+        Date.parse(params[:dashboard_date]).in_time_zone.change(hour: 22)
+      else
+        Time.zone.today
+      end
+
+
     @dashboard = Dashboard.new
     authorize @dashboard
 
@@ -9,54 +17,54 @@ class DashboardsController < ApplicationController
 
     @room_access_data = {
       "Not accessed for 3 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
-      .where('updated_at >= ?', 3.days.ago.beginning_of_day)
-      .where('updated_at < ?', 2.days.ago.beginning_of_day).count,
+      .where('updated_at >= ?', selected_date - 3.days)
+      .where('updated_at < ?', selected_date - 2.days).count,
 
       "Not accessed for 4 to 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
-      .where('updated_at >= ?', 7.days.ago.end_of_day)
-      .where('updated_at < ?', 3.days.ago.beginning_of_day).count,
+      .where('updated_at >= ?', selected_date - 7.days)
+      .where('updated_at < ?', selected_date - 3.days).count,
 
-      "Not accessed for over 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""]).where('updated_at < ?', 7.days.ago.beginning_of_day).count
+      "Not accessed for over 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""]).where('updated_at < ?', selected_date - 7.days).count
     }
 
     @room_check_in_data = {
-  "Not checked for 3 days": Room.joins(floor: :building)
-    .left_outer_joins(:room_states)
-    .where(buildings: { zone_id: !nil })
-    .where(room_states: { id: nil })
-    .or(
-      Room.joins(floor: :building)
-          .left_outer_joins(:room_states)
-          .where(buildings: { zone_id: !nil })
-          .where('room_states.created_at >= ?', 3.days.ago.beginning_of_day)
-          .where('room_states.created_at < ?', 2.days.ago.beginning_of_day)
-    )
-    .distinct.count,
+    "Not checked for 3 days": Room.joins(floor: :building)
+      .left_outer_joins(:room_states)
+      .where(buildings: { zone_id: !nil })
+      .where(room_states: { id: nil })
+      .or(
+        Room.joins(floor: :building)
+            .left_outer_joins(:room_states)
+            .where(buildings: { zone_id: !nil })
+            .where('room_states.created_at >= ?', selected_date - 3.days)
+            .where('room_states.created_at < ?', selected_date - 2.days)
+      )
+      .distinct.count,
 
-  "Not checked for 4 to 7 days": Room.joins(floor: :building)
-    .left_outer_joins(:room_states)
-    .where(buildings: { zone_id: !nil })
-    .where(room_states: { id: nil })
-    .or(
-      Room.joins(floor: :building)
-          .left_outer_joins(:room_states)
-          .where(buildings: { zone_id: !nil })
-          .where('room_states.created_at < ?', 4.days.ago.beginning_of_day)
-          .where('room_states.created_at >= ?', 7.days.ago.beginning_of_day)
-    )
-    .distinct.count,
+    "Not checked for 4 to 7 days": Room.joins(floor: :building)
+      .left_outer_joins(:room_states)
+      .where(buildings: { zone_id: !nil })
+      .where(room_states: { id: nil })
+      .or(
+        Room.joins(floor: :building)
+            .left_outer_joins(:room_states)
+            .where(buildings: { zone_id: !nil })
+            .where('room_states.created_at < ?', selected_date - 4.days)
+            .where('room_states.created_at >= ?', selected_date - 7.days)
+      )
+      .distinct.count,
 
-  "Not checked for over 7 days": Room.joins(floor: :building)
-    .left_outer_joins(:room_states)
-    .where(buildings: { zone_id: !nil })
-    .where(room_states: { id: nil })
-    .or(
-      Room.joins(floor: :building)
-          .left_outer_joins(:room_states)
-          .where(buildings: { zone_id: !nil })
-          .where('room_states.created_at < ?', 7.days.ago.beginning_of_day)
-    )
-    .distinct.count
+    "Not checked for over 7 days": Room.joins(floor: :building)
+      .left_outer_joins(:room_states)
+      .where(buildings: { zone_id: !nil })
+      .where(room_states: { id: nil })
+      .or(
+        Room.joins(floor: :building)
+            .left_outer_joins(:room_states)
+            .where(buildings: { zone_id: !nil })
+            .where('room_states.created_at < ?', selected_date - 7.days)
+      )
+      .distinct.count
 }
     
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -107,32 +107,6 @@ module ApplicationHelper
     end
   end
 
-  def room_check_status_class(checked, total)
-    # red for less than 50%
-    return 'text-danger' if checked < total * 0.5                                  
-    # orange for 50%-89%
-    return 'text-warning' if checked >= total * 0.5 && checked < total * 0.9        
-    # green for 90% and above
-    return 'text-success' if checked >= total * 0.9                                 
-    
-  end
-
-  def room_check_progress_class(checked, total)
-    # red for less than 50%
-    return 'bg-danger' if checked < total * 0.5                                  
-    # orange for 50%-89%
-    return 'bg-warning' if checked >= total * 0.5 && checked < total * 0.9        
-    # green for 90% and above
-    return 'bg-success' if checked >= total * 0.9                                
-  end
-
-
-  def completion_percentage(checked, total)
-    return 0 if total.zero?
-
-    ((checked.to_f / total) * 100).round
-  end
-
   def google_map_navigation_path(address)
     formatted_address = address.gsub(/\s/,'+')
     "https://www.google.com/maps/dir//" + formatted_address
