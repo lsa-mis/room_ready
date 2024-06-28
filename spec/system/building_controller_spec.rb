6532c9349a63a33962873e61c9da2dd021c86216
Mohammad Arjamand Ali <92491221+Mohammad4844@users.noreply.github.com>
2024-06-28 15:52:56 -0400

    Update building_controller_spec.rb

--- spec/system/building_controller_spec.rb
+++ spec/system/building_controller_spec.rb
@@ -23,26 +23,7 @@ RSpec.describe Building, type: :system do
     end
   end
 
-  context 'create a new building with valid bldrecnbr' do
-    it 'returns "New Building was added" message' do
-      VCR.use_cassette "building" do
-	sleep 7
-        bldrecnbr = '1234567'
-        result = {"success"=>true, "errorcode"=>"", "error"=>"", 
-          "data"=>[{"FiscalYear"=>2024, "BuildingRecordNumber"=>"1234567", "BuildingShortDescription"=>"BLAU HALL", 
-          "BuildingLongDescription"=>"BLAU JEFF T HALL", "BuildingStreetNumber"=>"700", "BuildingStreetDirection"=>"E", 
-          "BuildingStreetName"=>"UNIVERSITY AVE", "BuildingCity"=>"ANN ARBOR", "BuildingState"=>"MI", "BuildingPostal"=>"48109", 
-          "BuildingTypeDescription"=>"Teach, Research, Support", "BuildingPhaseCode"=>"SERV", "BuildingPhaseDescription"=>"In Service", 
-          "BuildingCampusCode"=>"100", "BuildingCampusDescription"=>"Central Campus", "BuildingOwnership"=>"Owned"}]}
-        allow_any_instance_of(BuildingApi).to receive(:get_building_info_by_bldrecnbr).with(bldrecnbr).and_return(result)
-        visit new_building_path
-        fill_in "Building Record Number", with: "1234567"
-        click_on "Create Building"
-	sleep 2
-        expect(page).to have_content("New Building was added")
-      end
-    end
-  end
+  
 
 	context 'edit a building' do
     it 'returns "bldrecnbr is invalid" message' do
