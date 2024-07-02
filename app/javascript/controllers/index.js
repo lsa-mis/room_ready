9784924c8d3ba364ba2a17d0f1d23bb6b31591fc
Satyadev Moolagani <smoolagani@gmail.com>
2024-07-02 12:08:40 -0400

    changed controller name to fix maps link

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,28 +2,28 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import AutosubmitController from "./autosubmit_controller"
-application.register("autosubmit", AutosubmitController)
+import AutosubmitController from "./autosubmit_controller";
+application.register("autosubmit", AutosubmitController);
 
-import BuildingController from "./building_controller"
-application.register("building", BuildingController)
+import BuildingController from "./building_controller";
+application.register("building", BuildingController);
 
-import ConfirmRedirectController from "./confirm_redirect_controller"
-application.register("confirm-redirect", ConfirmRedirectController)
+import ConfirmRedirectController from "./confirm_redirect_controller";
+application.register("redirect", ConfirmRedirectController);
 
-import FlashController from "./flash_controller"
-application.register("flash", FlashController)
+import FlashController from "./flash_controller";
+application.register("flash", FlashController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import HideelementController from "./hideelement_controller"
-application.register("hideelement", HideelementController)
+import HideelementController from "./hideelement_controller";
+application.register("hideelement", HideelementController);
 
-import RenderController from "./render_controller"
-application.register("render", RenderController)
+import RenderController from "./render_controller";
+application.register("render", RenderController);
 
-import RoomTicketsController from "./room_tickets_controller"
-application.register("room-tickets", RoomTicketsController)
+import RoomTicketsController from "./room_tickets_controller";
+application.register("room-tickets", RoomTicketsController);
