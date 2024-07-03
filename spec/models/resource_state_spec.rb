8824f48f581675bb47bf6e2c4ebc680d39cfac9f
Margarita Barvinok <brita@umich.edu>
2024-07-03 08:45:05 -0400

    add tests for readonly validation

--- spec/models/resource_state_spec.rb
+++ spec/models/resource_state_spec.rb
@@ -29,4 +29,13 @@ RSpec.describe ResourceState, type: :model do
       expect { FactoryBot.create(:resource_state, is_checked: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Is checked can't be blank")
     end
   end
+
+  context "edit old resource_state record" do
+    it 'is not valid' do
+      resource_state = FactoryBot.create(:resource_state)
+      resource_state.update(created_at: resource_state.created_at - 1.day, updated_at: resource_state.updated_at - 1.day)
+      expect(resource_state.update(is_checked: false)).to be_falsy
+      expect(resource_state.errors.full_messages_for(:base)).to include "Old resource state record cannot be edited"
+    end
+  end
 end
