78bb19bbec174cc888dcf2ac432fd96936aa8f82
Margarita Barvinok <brita@umich.edu>
2024-07-04 15:21:32 -0400

    move helpers methods from zone to helper file

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -12,27 +12,31 @@ class Zone < ApplicationRecord
   validates :name, presence: true
   has_many :buildings
 
-  def rooms_checked_today
-    Room.joins(floor: { building: :zone })
-        .includes(:room_states)
-        .where(zones: { id: self.id })
-        .select { |room| RoomStatus.new(room).room_checked_today? && RoomStatus.new(room).calculate_percentage.to_f == 100.0 }
-        .count
-  end
+  # def rooms_checked_today
+  #   Room.joins(floor: { building: :zone })
+  #       .includes(:room_states)
+  #       .where(zones: { id: self.id })
+  #       .select { |room| RoomStatus.new(room).room_checked_today? && RoomStatus.new(room).calculate_percentage.to_f == 100.0 }
+  #       .count
+  # end
 
-  def rooms_checked_for_date(selected_date)
-    date = Date.parse(selected_date)
-    RoomState.joins(room: { floor: { building: :zone } } )
-             .where(zones: { id: self.id })
-             .where(updated_at: date.beginning_of_day..date.end_of_day)
-             .select { |room_state| true } # RoomStatus calculate_percentage
-             .count
+  # def rooms_checked_for_date(selected_date)
+  #   if selected_date.present?
+  #     date = Date.parse(selected_date)
+  #   else
+  #     date = Date.today
+  #   end
+  #   RoomState.joins(room: { floor: { building: :zone } } )
+  #            .where(zones: { id: self.id })
+  #            .where(updated_at: date.beginning_of_day..date.end_of_day)
+  #            .select { |room_state| true } # RoomStatus calculate_percentage
+  #            .count
 
-  end
+  # end
 
-  def total_rooms
-    Room.joins(floor: { building: :zone })
-        .where(zones: { id: self.id })
-        .count
-  end
+  # def total_rooms
+  #   Room.joins(floor: { building: :zone })
+  #       .where(zones: { id: self.id })
+  #       .count
+  # end
 end
