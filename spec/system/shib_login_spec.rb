a044b00684572794951ad1afb2c2326994714168
Margarita Barvinok <brita@umich.edu>
2024-07-04 19:09:18 -0400

    edit login tests to work without network

--- spec/system/shib_login_spec.rb
+++ spec/system/shib_login_spec.rb
@@ -2,20 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Controllers", type: :request do
 
+  before do
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-developers').and_return(true)
+    allow(LdapLookup).to receive(:is_member_of_group?).with(anything, 'lsa-roomready-admins').and_return(false)
+  end
+
   describe 'login success' do
     it 'displays welcome message on programs page' do
-      user = FactoryBot.create(:user)
-      mock_login(user)
-      follow_redirect!
-      expect(response.body).to include("Signed in successfully.")
+      VCR.use_cassette "login" do
+        user = FactoryBot.create(:user)
+        mock_login(user)
+        follow_redirect!
+        expect(response.body).to include("Signed in successfully.")
+      end
     end
   end
 
   describe 'login failure' do
     it 'displays welcome message on programs page' do
-      user = FactoryBot.build(:user, email: "wrongemial")
-      mock_login(user)
-      expect(response.body).not_to include("Welcome")
+      VCR.use_cassette "login" do
+        user = FactoryBot.build(:user, email: "wrongemial")
+        mock_login(user)
+        expect(response.body).not_to include("Welcome")
+      end
     end
   end
 
