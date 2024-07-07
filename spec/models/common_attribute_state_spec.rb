accae4e3cfa524481513e9eaf853b3ea47b2627c
Margarita Barvinok <brita@umich.edu>
2024-07-07 00:09:11 -0400

    add new test

--- spec/models/common_attribute_state_spec.rb
+++ spec/models/common_attribute_state_spec.rb
@@ -13,5 +13,30 @@
 require 'rails_helper'
 
 RSpec.describe CommonAttributeState, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "the Factory" do
+    it 'is valid' do
+      expect(build(:common_attribute_states)).to be_valid
+    end
+  end
+
+  context "create common_attribute_states with all required fields present" do
+    it 'is valid' do
+      expect(create(:common_attribute_states)).to be_valid
+    end
+  end
+
+  context "create common_attribute_states without an is_checked value" do
+    it 'raise error "ActiveRecord::RecordInvalid: Validation failed: Is checked must be a present (either true or false)"' do
+      expect { FactoryBot.create(:common_attribute_states, is_checked: nil) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Is checked must be a present (either true or false)")
+    end
+  end
+
+  context "edit old common_attribute_states record" do
+    it 'is not valid' do
+      common_attribute_states = FactoryBot.create(:common_attribute_states)
+      common_attribute_states.update(created_at: common_attribute_states.created_at - 1.day, updated_at: common_attribute_states.updated_at - 1.day)
+      expect(common_attribute_states.update(is_checked: false)).to be_falsy
+      expect(common_attribute_states.errors.full_messages_for(:base)).to include "Old state record cannot be edited"
+    end
+  end
 end
