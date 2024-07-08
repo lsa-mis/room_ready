c95dc4ee7a32e20ebcc84f77821fc09b70e0012f
Satyadev Moolagani <smoolagani@gmail.com>
2024-07-08 16:55:55 -0400

    remove unused turbo thing

--- app/controllers/common_attributes_controller.rb
+++ app/controllers/common_attributes_controller.rb
@@ -12,13 +12,6 @@ class CommonAttributesController < ApplicationController
       @common_attributes = CommonAttribute.active
       @option_header = "Delete/Archive"
     end
-
-    respond_to do |format|
-      format.html
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace('common_attributes', partial: 'common_attributes_list', locals: { common_attributes: @common_attributes })
-      end
-    end
     
     @new_common_attribute = CommonAttribute.new
     authorize @common_attributes
