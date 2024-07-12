9626ef3099581aab8f32f91f91bcc243c82d7982
Margarita Barvinok <brita@umich.edu>
2024-07-12 10:46:55 -0400

    no accessed roms report for dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -28,16 +28,22 @@ class StaticPagesController < ApplicationController
     @zones = Zone.all.order(:name)
 
     @room_access_data = {
-      "Not accessed for 3 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
-      .where('updated_at >= ?', Date.today - 3.days)
-      .where('updated_at < ?', Date.today - 2.days).count,
-
-      "Not accessed for 4 to 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
-      .where('updated_at >= ?', Date.today - 7.days)
-      .where('updated_at < ?', Date.today - 3.days).count,
-
-      "Not accessed for over 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""]).where('updated_at < ?', Date.today - 7.days).count
+      "Not accessed for 3 days": rooms_not_accessed_for_number_of_days(3).count,
+      "Not accessed for 5 days": rooms_not_accessed_for_number_of_days(5).count,
+      "Not accessed for 7 days": rooms_not_accessed_for_number_of_days(7).count
     }
+
+    # @room_access_data = {
+    #   "Not accessed for 3 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
+    #   .where('updated_at >= ?', Date.today - 3.days)
+    #   .where('updated_at < ?', Date.today - 2.days).count,
+
+    #   "Not accessed for 4 to 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""])
+    #   .where('updated_at >= ?', Date.today - 7.days)
+    #   .where('updated_at < ?', Date.today - 3.days).count,
+
+    #   "Not accessed for over 7 days": RoomState.where(is_accessed: false).where.not(no_access_reason: [nil, ""]).where('updated_at < ?', Date.today - 7.days).count
+    # }
 
     @room_check_in_data = {
     "Not checked for 3 days": Room.joins(floor: :building)
@@ -79,6 +85,24 @@ class StaticPagesController < ApplicationController
       .distinct.count
 }
     
+  end
+
+  private
+  def rooms_not_accessed_for_number_of_days(number)
+    result_rooms = []
+    rooms = Room.active.joins(floor: :building)
+      .where.not(buildings: { zone_id: nil })
+    rooms.each do |room|
+
+      states = room.room_states.reverse.pluck(:is_accessed, :updated_at)[..number]
+      if states.length == number
+        result = Array.new(number, false) + [true]
+        if states.map { |item| item[0] } == result  && s[number][1].to_date == Date.today - number.day
+          result_rooms << room
+        end
+      end
+    end
+    return result_rooms
   end
 
 end
