e0ee0821f5cfb8a07878e67fc1d793f6b0f1b3d7
Margarita Barvinok <brita@umich.edu>
2024-11-26 10:48:33 -0500

    add turbo frame to dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,37 +15,40 @@ class StaticPagesController < ApplicationController
 
   def dashboard
     authorize :static_page
-
-    @selected_date = params[:dashboard_date].present? ? Date.parse(params[:dashboard_date]) : Date.today
     @zones = Zone.all.order(:name)
     active_rooms = Room.active_in_zones
-    
-    @room_access_data = {
-      "Not accessed 3 times": rooms_not_accessed_for_number_of_times(active_rooms, 3).count,
-      "Not accessed 5 times": rooms_not_accessed_for_number_of_times(active_rooms, 5).count,
-      "Not accessed 7 times": rooms_not_accessed_for_number_of_times(active_rooms, 7).count
-    }
 
-    @rooms_not_checked_in_3_days = active_rooms.where('DATE(last_time_checked) < ?', 3.days.ago.to_date)
-    @rooms_not_checked_4_to_7_days = active_rooms.where('DATE(last_time_checked) >= ? AND DATE(last_time_checked) < ?', 7.days.ago.to_date, 3.days.ago.to_date)
-    @rooms_not_checked_7_plus_days = active_rooms.where('DATE(last_time_checked) < ?', 7.days.ago.to_date)
-    @rooms_never_checked = active_rooms.where(last_time_checked: nil)
+    if params[:dashboard_date].present?
+      @selected_date = Date.parse(params[:dashboard_date])
+      render turbo_stream: turbo_stream.update("zones_overview", partial: "zones_overview")
+    else
+      @selected_date = Date.today
+      @room_access_data = {
+        "Not accessed 3 times": rooms_not_accessed_for_number_of_times(active_rooms, 3).count,
+        "Not accessed 5 times": rooms_not_accessed_for_number_of_times(active_rooms, 5).count,
+        "Not accessed 7 times": rooms_not_accessed_for_number_of_times(active_rooms, 7).count
+      }
 
-    @room_check_in_data = {
-      "Not checked for 3 days" => @rooms_not_checked_in_3_days.count,
-      "Not checked for 4 to 7 days" => @rooms_not_checked_4_to_7_days.count,
-      "Not checked for over 7 days" => @rooms_not_checked_7_plus_days.count,
-      "Never checked" => @rooms_never_checked.count
-    }
+      @rooms_not_checked_in_3_days = active_rooms.where('DATE(last_time_checked) < ?', 3.days.ago.to_date)
+      @rooms_not_checked_4_to_7_days = active_rooms.where('DATE(last_time_checked) >= ? AND DATE(last_time_checked) < ?', 7.days.ago.to_date, 3.days.ago.to_date)
+      @rooms_not_checked_7_plus_days = active_rooms.where('DATE(last_time_checked) < ?', 7.days.ago.to_date)
+      @rooms_never_checked = active_rooms.where(last_time_checked: nil)
 
-    last_room_states = RoomState.all.order(updated_at: :desc).limit(5)
-    @last_checked_rooms = last_room_states.map { |room_state| [Room.find(room_state.room_id), room_state.updated_at] }
+      @room_check_in_data = {
+        "Not checked for 3 days" => @rooms_not_checked_in_3_days.count,
+        "Not checked for 4 to 7 days" => @rooms_not_checked_4_to_7_days.count,
+        "Not checked for over 7 days" => @rooms_not_checked_7_plus_days.count,
+        "Never checked" => @rooms_never_checked.count
+      }
 
-    @latest_room_tickets = RoomTicket.latest
+      last_room_states = RoomState.all.order(updated_at: :desc).limit(5)
+      @last_checked_rooms = last_room_states.map { |room_state| [Room.find(room_state.room_id), room_state.updated_at] }
 
-    @room_update_log = RoomUpdateLog.last
-    @room_update_log_status = @room_update_log&.status
-    
+      @latest_room_tickets = RoomTicket.latest
+
+      @room_update_log = RoomUpdateLog.last
+      @room_update_log_status = @room_update_log&.status
+    end
   end
 
   private
