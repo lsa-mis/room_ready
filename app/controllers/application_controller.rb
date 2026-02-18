493e9af755744add7b80c2aa104f810691a73b91
Margarita Barvinok <brita@umich.edu>
2026-02-18 10:30:18 -0500

    cover turbo format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
 
   def render_404
     respond_to do |format|
-      format.html { render 'errors/not_found', status: :not_found, layout: 'error' }
+      format.any { render 'errors/not_found', status: :not_found, layout: 'error' }
       format.json { render json: { error: 'Not Found' }, status: :not_found }
     end
   end
@@ -70,7 +70,7 @@ class ApplicationController < ActionController::Base
   def render_500(exception)
     # Log the error, send it to error tracking services, etc.
     respond_to do |format|
-      format.html { render 'errors/internal_server_error', status: :internal_server_error, layout: 'error' }
+      format.any { render 'errors/internal_server_error', status: :internal_server_error, layout: 'error' }
       format.json { render json: { error: 'Internal Server Error' }, status: :internal_server_error }
     end
   end
